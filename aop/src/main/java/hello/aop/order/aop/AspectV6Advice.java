e16fcc1f2f042615ffd3b0f638ae10d904efb82d
Seoyun <newseoyun@gmail.com>
2022-03-21 22:56:13 +0900

    스프링 AOP 구현

---
+++ aop/src/main/java/hello/aop/order/aop/AspectV6Advice.java
@@ -0,0 +1,54 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+@Slf4j
+@Aspect
+public class AspectV6Advice {
+
+    // hello.aop.order 패키지와 하위 패키지이면서 클래스 이름 패턴이 *Service
+//    @Around("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            // @Before
+            log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
+            Object result = joinPoint.proceed();
+            // @AfterReturning
+            log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+            return result;
+        } catch (Exception e) {
+            // @AfterThrowing
+            log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
+            throw e;
+        } finally {
+            // @After
+            log.info("[트랜잭션 릴리즈] {}", joinPoint.getSignature());
+        }
+    }
+
+    // 아래 아이들은 JoinPoint 파라미터 생략 가능
+
+    @Before("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void doBefore(JoinPoint joinPoint) {
+        log.info("[before] {}", joinPoint.getSignature());
+    }
+
+    @AfterReturning(value = "hello.aop.order.aop.Pointcuts.orderAndService()", returning = "result")
+    public void doReturn(JoinPoint joinPoint, Object result) {
+        log.info("[afterReturning] {} return={}", joinPoint.getSignature(), result);
+    }
+
+    @AfterThrowing(value = "hello.aop.order.aop.Pointcuts.orderAndService()", throwing = "ex")
+    public void doThrowing(JoinPoint joinPoint, Throwable ex) {
+        log.info("[AfterThrowing] {} return={}", joinPoint.getSignature(), ex);
+    }
+
+    @After(value = "hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void doAfter(JoinPoint joinPoint) {
+        log.info("[after]", joinPoint);
+    }
+
+}
